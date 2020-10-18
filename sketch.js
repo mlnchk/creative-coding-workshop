06a134341cbb562f6c4cc6b5f32ba5dafaf60c3a
mlnchk <danil.mlnchk@gmail.com>
2020-10-19 01:31:07 +0300

    noise and rotation stuff

--- sketch.js
+++ sketch.js
@@ -18,18 +18,21 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v) * 0.05);
+        const rotation = random.noise2D(u, v);
         points.push({
           color: random.pick(pallete),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
           position: [u, v],
+          radius,
+          rotation,
         });
       }
     }
     return points;
   };
 
-  random.setSeed(512);
-  const points = createGrid().filter(() => random.value() > 0.5);
+  // random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.1);
   const margin = 400;
 
   return ({ context, width, height }) => {
@@ -37,17 +40,27 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     points.forEach((data) => {
-      const { position, radius, color } = data;
+      const { position, radius, color, rotation } = data;
       const [u, v] = position;
       // const x = u * width;
       // const y = v * height;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
+
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px 'Helvetica'`;
+      context.translate(x, y); // меняет опорную точку канваса
+      context.rotate(rotation);
+      context.fillText("#", 0, 0); // 0, 0 потому что мы сделали translate
+
+      context.restore();
     });
   };
 };
