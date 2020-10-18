084dd3db2e1a0fd017394e42da1fc46702202325
mlnchk <danil.mlnchk@gmail.com>
2020-10-18 12:11:37 +0300

    grid stuff

---
+++ sketch.js
@@ -0,0 +1,46 @@
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+
+const settings = {
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 40;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(([u, v]) => {
+      // const x = u * width;
+      // const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 5, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = 20;
+      context.stroke();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
